3cd383e260ba57bfff1d35d7b21f95dfa1245d3d
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-04-19 07:28:19 -0500

    Minor changes

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -9,30 +9,12 @@ namespace Wordle
         {
             var regina = new Regina();
 
-            var game = new WordleGame("flair");
+            var game = new WordleGame("foyer");
             game.MaxGuesses = 20;
 
             int guesses = game.Play(regina);
 
             Console.WriteLine(guesses);
-
-            //GuessResult result = game.CheckGuess("rater");
-            //Console.WriteLine(result);
-
-            /*char c = 'x';
-            string pattern = $@"{c}";
-            pattern += @".";
-            pattern += @".";
-            pattern += @"[^n]";
-            pattern += @"e";
-
-            Regex rgx = new Regex(pattern);
-            Console.WriteLine(rgx);*/
-
-            /*string word = "cider";
-            Regex keepers = new Regex(@"(?=.*c)(?=.*i)(?=.*n)");
-            Console.WriteLine(keepers);
-            Console.WriteLine(keepers.IsMatch(word));*/
         }
     }
 }
