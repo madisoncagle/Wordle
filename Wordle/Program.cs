2f150a05c9849203b7c69cc6ec1129e0380b9a1e
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-11-11 08:28:45 -0600

    Play new words
    I never should've mentioned anything about the file of shame. I jinxed myself.

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -15,7 +15,7 @@ namespace Wordle
             string dp = "../../../data";
 
             // play regina
-            var word = "spell";
+            var word = "medal";
             int reginaScore = PlayRegina(word);
 
             // get my score
