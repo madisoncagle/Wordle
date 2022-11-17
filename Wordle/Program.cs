df11dac815a001494a0da1bc7498ef4bfc1010f0
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-11-17 12:18:47 -0600

    Play through Wordle #516
    Lose a few times

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -15,7 +15,7 @@ namespace Wordle
             string dp = "../../../data";
 
             // play regina
-            var word = "medal";
+            var word = "there";
             int reginaScore = PlayRegina(word);
 
             // get my score
