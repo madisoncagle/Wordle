0618913af9636b6008f7ac378bc68934b85be9c7
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2023-01-27 08:23:05 -0600

    New word

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -15,7 +15,7 @@ namespace Wordle
             string dp = "../../../data";
 
             // play regina
-            var word = "beefy";
+            var word = "worry";
             int reginaScore = PlayRegina(word);
 
             // get my score
