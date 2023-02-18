095d42a83dc2a64bbe9dd2b8a70046faea023f7f
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2023-02-18 12:52:28 -0600

    New words
    She beat me on giant, seriously?

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -15,7 +15,7 @@ namespace Wordle
             string dp = "../../../data";
 
             // play regina
-            var word = "debug";
+            var word = "avail";
             int reginaScore = PlayRegina(word);
 
             // get my score
